a815c8de45250b05a55966c3ad50ad0a319859f8
Vasilij Aslamov <wasobad@mail.ru>
2025-01-04 16:09:11 +0300

    Architecture changed

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import telebot
 
+bot = telebot.TeleBot("7764412824:AAEviLrXcbvSV0gtotXcByCWp65qF3zivgs") # Токен, полученный от BotFather.
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Hello world!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+if __name__ == "__main__":
+    bot.infinity_polling()
