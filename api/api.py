a3c5b864482acba37dc2daf2fe8678276aabac0b
Vasilij Aslamov <wasobad@mail.ru>
2025-01-06 09:33:27 +0300

    greetings added

---
+++ api/api.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://livescore6.p.rapidapi.com/leagues/v2/list"
+
+querystring = {"Category":"soccer"}
+
+headers = {
+	"x-rapidapi-key": "ec925d340cmsh274017ddced15cep1715cbjsnb3f1eb6cac5f",
+	"x-rapidapi-host": "livescore6.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import telebot
 
-bot = telebot.TeleBot("7764412824:AAEviLrXcbvSV0gtotXcByCWp65qF3zivgs") # Токен, полученный от BotFather.
+bot = telebot.TeleBot("7653689159:AAFTfZVYJ2Icdt--vm30zXbSLMViLQNBW9E")
 
-@bot.message_handler(commands=["start", "help"])
-def send_welcome(message):
-    bot.reply_to(message, "Hello world!")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=["start", "main", "hello"])
+def main(message):
+    bot.reply_to(message, f"Привет {message.from_user.first_name}! Я бот, который поможет тебе найти "
+                          "информацию о матчах Английской Премьер-лиги. Выберите дату матча:")
+
+
+@bot.message_handler(commands=["help"])
+def main(message):
+    bot.reply_to(message, "Для начала нужно выбрать дату или ввести ее в формате дд-мм-гггг. Например: 28-04-1989")
+
+
+@bot.message_handler()
+def greetings(message):
+    if message.text.lower() in ['привет', 'hi', 'здорова', 'здравствуйте']:
+        bot.reply_to(message, f"Привет {message.from_user.first_name}! Я бот, который поможет тебе найти "
+                              "информацию о матчах Английской Премьер-лиги. Выберите дату матча:")
+
 
 if __name__ == "__main__":
     bot.infinity_polling()
